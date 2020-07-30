cb7490a2d8a17be5ea4a74f00fabfd50c4758a5d
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-30 10:55:52 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -15,6 +15,7 @@ def create_project_hash
   # return the projects hash
   projects
 end
+
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
