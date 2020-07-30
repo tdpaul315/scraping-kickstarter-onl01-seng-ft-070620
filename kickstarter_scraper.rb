9caeec3aade9bf4709f4d7bc552f886975be29ab
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-30 10:37:26 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,7 +1,7 @@
 require 'nokogiri' # require libraries/modules here
 
 def create_project_hash
-  File.read('fixtures/kickstarter.html') # write your code here
+  html = File.read('fixtures/kickstarter.html') # write your code here
   
   kickstarter = Nokogiri::HTML(html)
 end
